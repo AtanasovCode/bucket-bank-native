074c63a9aa8405468dcfdd84814d15982a894f83
AtanasovCode <a_ljupce@yahoo.com>
2024-05-10 23:55:32 +0200

    created payments header

--- components/bucket/Payments.jsx
+++ components/bucket/Payments.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import {
     View,
     Text,
+    Image,
 } from "react-native";
 import { StyleSheet, Dimensions } from "react-native";
+import { theme } from "../../Colors";
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
@@ -11,7 +13,12 @@ const height = Dimensions.get("window").height;
 const Payments = ({ navigation }) => {
     return (
         <View style={[styles.container, { width: width }]}>
-            <Text>Overview</Text>
+            <View style={[styles.wrapper]}>
+                <Image source={require('../../assets/history.png')} style={{width: 35, height: 35,}} />
+                <Text style={[styles.text, {color: theme.light}]}>
+                    History
+                </Text>
+            </View>
         </View>
     );
 }
@@ -20,6 +27,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    wrapper: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 16,
+    },
+    text: {},
+    title: {},
 })
 
 export default Payments;
