b70cddc396522938cce491e49c18d23663c00feb
AtanasovCode <a_ljupce@yahoo.com>
2024-05-11 21:23:13 +0200

    added styling to rendered payments

--- components/bucket/Payments.jsx
+++ components/bucket/Payments.jsx
@@ -10,6 +10,7 @@ import {
 import { StyleSheet, Dimensions } from "react-native";
 import { theme } from "../../Colors";
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import { formatMoney } from "../Utils";
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
@@ -27,22 +28,37 @@ const Payments = ({ navigation, bucket, setBucket, }) => {
                 </Text>
             </View>
             <View>
-            {
-                bucket.payments && bucket.payments.length > 0 ?
-                bucket.payments.map((item) => {
-                    return (
-                        <View key={item.id}>
-                            <Text style={{color: theme.text}}>{item.amount}</Text>
+                {
+                    bucket.payments && bucket.payments.length > 0 ?
+                        bucket.payments.map((item) => {
+                            return (
+                                <View key={item.id} style={[styles.paymentContainer]}>
+                                    <View style={[styles.paymentWrapper]}>
+                                        <Image
+                                            source={require('../../assets/save.png')}
+                                            style={{ width: 35, height: 35, }}
+                                        />
+                                        <Text style={[styles.text, { color: theme.text }]}>{item.date}</Text>
+                                    </View>
+                                    <View>
+                                        <Text style={[styles.text, {color: theme.money, fontFamily: "monospace"}]}>
+                                            {formatMoney(item.amount)} $
+                                        </Text>
+                                    </View>
+                                </View>
+                            );
+                        })
+                        :
+                        <View>
+                            <Image
+                                source={require('../../assets/save.png')}
+                                style={{ width: 25, height: 25, }}
+                            />
+                            <Text style={{ color: theme.text }}>
+                                No payments found
+                            </Text>
                         </View>
-                    );
-                })
-                :
-                <View>
-                    <Text style={{color: theme.text}}>
-                        No payments found
-                    </Text>
-                </View>
-            }
+                }
             </View>
             <View style={[styles.addContainer, { backgroundColor: theme.background }]}>
                 <TouchableHighlight
@@ -69,7 +85,21 @@ const styles = StyleSheet.create({
         gap: 16,
         marginBottom: "10%",
     },
-    text: {},
+    text: {
+        fontSize: 14,
+    },
+    paymentContainer: {
+        flexDirection: "row",
+        marginBottom: "5%",
+        marginLeft: "6%",
+        marginRight: "6%",
+        justifyContent: "space-between"
+    },
+    paymentWrapper: {
+        flexDirection: "row",
+        gap: 22,
+        alignItems: "center",
+    },
     title: {},
     addContainer: {
         padding: 26,
