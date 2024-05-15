31fd3f16d2ad363a9d69139e3063f43331a96fcc
AtanasovCode <a_ljupce@yahoo.com>
2024-05-15 13:19:44 +0200

    improved picker in settings

--- routes/Settings.jsx
+++ routes/Settings.jsx
@@ -15,10 +15,34 @@ import { Picker } from "@react-native-picker/picker";
 const Settings = ({ navigation }) => {
 
     const [selectedTheme, setSelectedTheme] = useState("automatic");
+    const [selectedCurrency, setSelectedCurrency] = useState("dollar");
 
     const colorScheme = useColorScheme();
 
     const theme = colorScheme === "light" ? lightTheme : darkTheme;
+
+    const [currencies, setCurrencies] = useState([
+        { label: "US Dollar (USD) - $", value: "$" },
+        { label: "Euro (EUR) - €", value: "€" },
+        { label: "Japanese Yen (JPY) - ¥", value: "¥" },
+        { label: "British Pound Sterling (GBP) - £", value: "£" },
+        { label: "Australian Dollar (AUD) - A$", value: "A$" },
+        { label: "Canadian Dollar (CAD) - CA$", value: "CA$" },
+        { label: "Swiss Franc (CHF) - CHF", value: "CHF" },
+        { label: "Swedish Krona (SEK) - SEK", value: "SEK" }, 
+        { label: "New Zealand Dollar (NZD) - NZ$", value: "NZ$" },
+        { label: "South Korean Won (KRW) - ₩", value: "₩" },
+        { label: "Singapore Dollar (SGD) - S$", value: "S$" },
+        { label: "Mexican Peso (MXN) - Mex$", value: "Mex$" },
+        { label: "Indian Rupee (INR) - ₹", value: "₹" },
+        { label: "Brazilian Real (BRL) - R$", value: "R$" },
+        { label: "Thai Baht (THB) - ฿", value: "THB" }, 
+        { label: "Indonesian Rupiah (IDR) - Rp", value: "IDR" },
+        { label: "UAE Dirham (AED) - AED", value: "AED" },
+        { label: "Philippine Peso (PHP) - ₱", value: "PHP" },
+        { label: "Polish Złoty (PLN) - zł", value: "PLN" },
+    ]);
+
 
     return (
         <SafeAreaView style={[styles.container, { backgroundColor: theme.background }]}>
@@ -32,31 +56,61 @@ const Settings = ({ navigation }) => {
                     <Text style={[styles.name, { color: theme.text }]}>
                         Theme
                     </Text>
-                    <Picker
-                        style={[styles.picker,
-                        { backgroundColor: theme.inactive, color: theme.text}]}
-                        selectedValue={selectedTheme}
-                        mode="dropdown"
-                        onValueChange={(itemValue, itemIndex) =>
-                            setSelectedTheme(itemValue)
-                        }
-                    >
-                        <Picker.Item
-                            label="Automatic"
-                            value="automatic"
-                            style={{ backgroundColor: theme.inactive, color: theme.light }}
-                        />
-                        <Picker.Item
-                            label="Dark"
-                            value="dark"
-                            style={{ backgroundColor: theme.inactive, color: theme.light }}
-                        />
-                        <Picker.Item
-                            label="Light"
-                            value="light"
-                            style={{ backgroundColor: theme.inactive, color: theme.light }}
-                        />
-                    </Picker>
+                    <View style={[styles.pickerWrapper]}>
+                        <Picker
+                            style={[styles.picker,
+                            { backgroundColor: colorScheme === "dark" ? theme.inactive : theme.inactiveLighter, color: theme.text }]}
+                            selectedValue={selectedTheme}
+                            mode="dropdown"
+                            onValueChange={(itemValue, itemIndex) =>
+                                setSelectedTheme(itemValue)
+                            }
+                        >
+                            <Picker.Item
+                                label="Automatic"
+                                value="automatic"
+                                style={{ backgroundColor: colorScheme === "dark" ? theme.inactive : theme.inactiveLighter, color: theme.light }}
+                            />
+                            <Picker.Item
+                                label="Dark"
+                                value="dark"
+                                style={{ backgroundColor: colorScheme === "dark" ? theme.inactive : theme.inactiveLighter, color: theme.light }}
+                            />
+                            <Picker.Item
+                                label="Light"
+                                value="light"
+                                style={{ backgroundColor: colorScheme === "dark" ? theme.inactive : theme.inactiveLighter, color: theme.light }}
+                            />
+                        </Picker>
+                    </View>
+                </View>
+                <View style={[styles.settingWrapper]}>
+                    <Text style={[styles.name, { color: theme.text }]}>
+                        Currency
+                    </Text>
+                    <View style={[styles.pickerWrapper]}>
+                        <Picker
+                            style={[styles.picker,
+                            { backgroundColor: colorScheme === "dark" ? theme.inactive : theme.inactiveLighter, color: theme.text }]}
+                            selectedValue={selectedCurrency}
+                            mode="dropdown"
+                            onValueChange={(itemValue, itemIndex) =>
+                                setSelectedCurrency(itemValue)
+                            }
+                        >
+                            {
+                                currencies.map((currency) => {
+                                    return (
+                                        <Picker.Item
+                                            label={currency.label}
+                                            value={currency.value}
+                                            style={{ backgroundColor: colorScheme === "dark" ? theme.inactive : theme.inactiveLighter, color: theme.light }}
+                                        />
+                                    );
+                                })
+                            }
+                        </Picker>
+                    </View>
                 </View>
             </View>
         </SafeAreaView>
@@ -80,15 +134,19 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
-        marginBottom: "6%",
+        marginBottom: "20%",
     },
     name: {
         flex: 1,
         fontSize: 15,
     },
-    picker: {
+    pickerWrapper: {
         flex: 1,
         borderRadius: 12,
+        overflow: "hidden",
+    },
+    picker: {
+        flex: 1,
     },
 })
 
