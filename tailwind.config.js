931c588842fba8af3b985273b0757dd46822cdb5
AtanasovCode <a_ljupce@yahoo.com>
2024-05-29 22:39:25 +0200

    installed nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./components/bucket/**/*.{js,jsx,ts,tsx}",
+    "./components/dashboard/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/routes/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
