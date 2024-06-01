1fe5a6af0397e015e9740bbccfe8255063336e51
AtanasovCode <a_ljupce@yahoo.com>
2024-06-01 12:48:01 +0200

    updated store

--- useStore.jsx
+++ useStore.jsx
@@ -2,7 +2,9 @@ import { create } from "zustand";
 
 const useStore = create(set => ({
     currency: "$",
+    changeCurrency: (value) => set((state) => ({
+        currency: value
+    }))
 }));
-
 
 export default useStore;
